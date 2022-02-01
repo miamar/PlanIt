acc2a1dd2c609116e237dcdf8e2c95e249fa1c90
Mia Markanovic <mia.markanovic16@gmail.com>
2022-02-01 22:00:48 +0100

    login

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const ejs = require('ejs');
+const passport = require('passport');
+const session = require('express-session');
 var express = require('express');
 app.set('view engine', 'ejs');
 
 const User = require('./models/User.js');
 
 var app = express();
+
 
 /* SPAJANJE NA SERVER */
 app.listen(3030, () => {
@@ -19,10 +22,44 @@ mongoose.connect('mongodb+srv://anaanic:ana123@planitcluster.bifgt.mongodb.net/m
     console.log('Connected to Mongo DB Successfully!!');
  })
 
- app.use(bodyParser.urlencoded({ extended: true }));
- app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
- const router = express.Router();
+const router = express.Router();
+
+router.use(session({
+	secret: 'any key is fine',
+	resave: true,
+	saveUninitialized: true,
+}))
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+   { usernameField: "userName" },
+   (userName, password, done) => {
+     User.findOne({userName: userName}, (err, userData) => {
+      let passwordCheck = bcrypt.compareSync(password, userData.password);
+	  if (userName === userData.userName && passwordCheck) {
+		  return done(null, userData)
+		  }else{
+			  return done(null, false, { message: 'Korisnik ne postoji.'})
+		  }
+	    })
+    }
+));
+
+router.post('/login', (req, res, next) => {
+	passport.authenticate('local', (err, user, info) => {
+		 req.login(user, (err) => {
+		 res.redirect('/index');
+		 })
+	})
+	(req, res, next);
+})
 
  router.post('/user', (request, response) => {
 	const user = new User({
@@ -44,6 +81,19 @@ mongoose.connect('mongodb+srv://anaanic:ana123@planitcluster.bifgt.mongodb.net/m
 	  })
   })
 }) 
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+	console.log("Serializing user: ", user);
+});
+
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function(err, user) {
+		loggedInUser = user;
+		done(err, user);
+	});
+	console.log("Deserializing user:", loggedInUser);
+});
 
 router.get('/signup', function(req, res){
 	res.sendFile(path.join(__dirname + '/views' + '/registracija.html'));
