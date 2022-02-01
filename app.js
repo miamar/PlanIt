36e38faee140f1f16a244b0e0d764e2327273d26
Mia Markanovic <mia.markanovic16@gmail.com>
2022-02-01 21:42:01 +0100

    registracija

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const ejs = require('ejs');
 var express = require('express');
+app.set('view engine', 'ejs');
 
 const User = require('./models/User.js');
 
@@ -38,9 +40,15 @@ mongoose.connect('mongodb+srv://anaanic:ana123@planitcluster.bifgt.mongodb.net/m
 		  //response.redirect("/");
 	  }).catch(error => {
 		  console.log("Error - user not saved!");
-		  //response.redirect("/signup");
+		  response.redirect("/signup");
 	  })
   })
 }) 
+
+router.get('/signup', function(req, res){
+	res.sendFile(path.join(__dirname + '/views' + '/registracija.html'));
+});
+
+
 module.exports = router;
  
